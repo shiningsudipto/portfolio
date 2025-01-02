1988a62d6ce1cd95725ae46f2d758ac48125bd52
Sudipta Das <shiningsudipto@gmail.com>
2025-01-02 18:55:52 +0600

    important links added

--- src/components/modules/home/Banner.tsx
+++ src/components/modules/home/Banner.tsx
@@ -1,23 +1,40 @@
 import avatar from "@/assets/sudipta-das.jpg";
 import Image from "next/image";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 const Banner = () => {
   return (
     <div
       id="banner"
       className="space-xy flex lg:flex-row flex-col justify-between items-center bg-black lg:h-[90vh] gap-y-10"
     >
-      <div className="space-y-4">
+      <div className="">
         <h1 className="lg:text-5xl text-3xl">
           I&apos;m <span className="text-primary">Sudipta Das</span>
         </h1>
-        <h2 className="lg:text-3xl text-2xl font-bold">Web Developer</h2>
-        <h3 className="lg:text-xl text-lg font-medium">
+        <h2 className="lg:text-3xl text-2xl font-bold my-3">Web Developer</h2>
+        <h3 className="lg:text-xl text-lg font-medium mb-4">
           Passionate about crafting impactful web experiences.
         </h3>
-        <div className="">
+        <div className="flex items-center gap-2 text-3xl">
+          <a
+            href="https://www.linkedin.com/in/sudiptadasweb"
+            target="_blank"
+            className="text-primary hover:text-primary-600"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/shiningsudipto"
+            target="_blank"
+            className="text-primary hover:text-primary-600"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
+        <div className="mt-7">
           <a
             href="#contact"
-            className="text-primary border-2 border-primary px-6 py-2 font-semibold"
+            className="text-primary border-2 border-primary hover:border-primary-600 px-6 py-2 font-semibold"
           >
             Contact me
           </a>
--- src/components/modules/home/Contact.tsx
+++ src/components/modules/home/Contact.tsx
@@ -9,18 +9,18 @@ const Contact = () => {
       <h2 className="heading">Contact Me</h2>
       <div className="mt-10 flex flex-col justify-center items-center gap-5">
         <div className="flex items-center lg:flex-row flex-col justify-center gap-5">
-          <p className="flex items-center gap-2 text-xl bg-black w-[320px] px-4 py-2 rounded-md">
+          <p className="flex items-center gap-2 text-xl bg-black min-w-[320px] px-4 py-2 rounded-md">
             <TbMail className="mb-1 text-2xl text-primary" />
             <a href="mailto:shiningsudipto@gmail.com">
               shiningsudipto@gmail.com
             </a>
           </p>
-          <p className="flex items-center gap-2 text-xl bg-black w-[320px] px-4 py-2 rounded-md">
+          <p className="flex items-center gap-2 text-xl bg-black min-w-[320px] px-4 py-2 rounded-md">
             <TbPhone className="mb-1 text-2xl text-primary" />
             <a href="tel:+8801758579417">01758579417</a>
           </p>
         </div>
-        <p className="flex items-center gap-2 text-xl bg-black w-[320px] py-2 px-4 rounded-md">
+        <p className="flex items-center gap-2 text-xl bg-black min-w-[320px] py-2 px-4 rounded-md">
           <IoLocationOutline className="mb-1 text-2xl text-primary" />
           Barishal, Bangladesh
         </p>
